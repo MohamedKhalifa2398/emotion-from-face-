9443ddfdd986c1354cafd6a523e004ded41b5cc7
LAPTOP-VM2OIF0U\Muhhamed Tarek <tarek.mt1998@gmail.com>
2019-09-09 23:13:22 +0200

    resolve issues and add some comments to code

--- CNN LSTM.py
+++ CNN LSTM.py
@@ -16,39 +16,31 @@ Created on Thu Jul 18 15:23:43 2019
 import time
 import os
 import cv2
-#import pandas as pd
 import numpy as np
-#from PIL import Image
-#import tensorflow as tf
 from sklearn.model_selection import train_test_split
-
-
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Activation, Flatten ,TimeDistributed ,LSTM 
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
-#from keras.losses import categorical_crossentropy
-#from keras.optimizers import Adam
-#from keras.regularizers import l2
-#from keras.callbacks import ReduceLROnPlateau, TensorBoard, EarlyStopping, ModelCheckpoint
-#from keras.models import load_model
+from keras.layers import Conv2D, MaxPooling2D
+
+
+
+train_file = "Train"
+test_file = "Test"
 
 
 
 
 
 
-
-
-
-lst = []
-X=np.array([])
-Y = []
-start_time = time.time()
-
-for subdir, dirs, files in os.walk("DATASET"):
-  print(subdir, dirs)
-  for file in files:
+def load(file_name):
+ X=np.array([])
+ Y = []
+ start_time = time.time()
+ for subdir, dirs, files in os.walk(file_name):
+   print(subdir, dirs)
+   for file in files:
         fra=[]
+        label = [0,0,0,0,0,0,0,0]
         cap = cv2.VideoCapture(subdir+"\\"+file)
         cap.set(cv2.CAP_PROP_POS_FRAMES,(cap.get(cv2.CAP_PROP_FRAME_COUNT)/2)-44)
         print(cap.get(cv2.CAP_PROP_FRAME_COUNT),"---",cap.get(cv2.CAP_PROP_POS_FRAMES))
@@ -56,42 +48,32 @@ for subdir, dirs, files in os.walk("DATASET"):
             ret , frame = cap.read()
             
             gray =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            #face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-            #faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-            #for (x, y, w, h) in faces:
-             
-             #roi_gray = gray[y:y + h, x:x + w]
-             #cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_gray, (56, 56)), -1), 0)
-             #cv2.imshow("F",cv2.resize(roi_gray, (120, 120)))
-             #cv2.waitKey(0)
+            #cv2.imshow("a",frame)
+            #cv2.waitKey(0)
             if o%2==0: 
                 fra.append(cv2.resize(gray, (56, 56)))
             
         
         # The instruction below converts the labels (from 1 to 8) to a series from 0 to 7
         # This is because our predictor needs to start from 0 otherwise it will try to predict also 0.
-        file = int(file[7:8]) - 1 
-        arr = fra, file
+        label[int(file[7:8]) - 1] = 1 
         
-        lst.append(arr)
-        Y.append(file)
+        
+        
+        Y.append(label)
         X = np.append(X,np.array(fra) )
-        print(np.array(lst).shape)
-      # If the file is not valid, skip it
+        
+ print("--- Data loaded. Loading time: ", (time.time() - start_time), "secounds from ",file_name)       
+ return X ,Y
+ 
+
+
+X_train , y_train = load(train_file)
+X_test , y_test = load(test_file)
 
 
 
-
-
-
-print("--- Data loaded. Loading time: %s seconds ---" % (time.time() - start_time))
-
-
-
-
-
-
-
+'''
 
 
 print ("building model…")
@@ -121,7 +103,6 @@ print (len(Y))
 
 
 X = X.reshape(-1,44,56,56,1)
-X_train, X_test, y_train, y_test =train_test_split( X, Y, test_size=0.33, random_state=42)
 
 
 X_train = np.array(X_train)
@@ -148,3 +129,4 @@ show_accuracy=True)
 print("Test score:", score)
 print("Test accuracy:", acc)
 
+'''
